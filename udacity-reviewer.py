a1a5e69b3e215fa7204a2aa4dc972b01acabd70f
Kartik Arora <chipset95@yahoo.co.in>
2017-02-06 17:33:44 +0530

    Port

--- udacity-reviewer.py
+++ udacity-reviewer.py
@@ -41,5 +41,6 @@ def register():
     return response.text
 
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(port), threaded=True)
